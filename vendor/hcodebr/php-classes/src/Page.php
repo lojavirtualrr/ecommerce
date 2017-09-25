61fd4a00df70824d3ff3ec5a66a58948b6ea2639
lojavirtualrr <contato@lojavirtualrr.com.br>
2017-09-25 14:31:58 -0300

    criando o pageadmin e o tpl admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -13,12 +13,12 @@ class Page {
 
   ];
 
-   public function __construct($opts = array()){
+   public function __construct($opts = array(), $tpl_dir = "/views/"){
 
-   $this->$options = array_merge($this->$defaults, $opts);
+   $this->options = array_merge($this->defaults, $opts);
 
    $config = array (
-     "tpl_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views/",
+     "tpl_dir"     => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
      "cache_dir"   => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
      "debug"       => false
    );
@@ -27,7 +27,7 @@ class Page {
 
     $this->tpl = new Tpl;
 
-    $this->setData($options["data"]);
+    $this->setData($this->options["data"]);
 
     $this->tpl->draw("header");
 
